ad5c27eeb094eebe55350eabde4f2454a8d8b275
frederico <fredfredhouse321@hotmail.com>
2021-09-22 20:46:33 -0300

    att

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ hamburguer.addEventListener('click', function(){
     document.querySelector('.container').classList.toggle('show-menu');
 })
 
+var scrollTop = document.getElementById('link-topo').innerHTML
+
+scrollTop = function scrll() {
+    window.scrollTo(0, 0);
+};
+
 
